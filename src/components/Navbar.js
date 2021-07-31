20a4a7bc0b6045d741af7f060fa43ad28348cb68
Domenic Labbate <domeniclabbate@gmail.com>
2021-07-31 00:22:45 -0400

    made the UI more responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
+import { FiMenu, FiX } from "react-icons/fi";
 
 const Navbar = () => {
+  const [menuClicked, setMenuClicked] = useState(false);
+
+  const toggleMenuClick = () => {
+    setMenuClicked(!menuClicked);
+  };
+
   const navbarLinks = [
     { url: "#", title: "Home" },
     { url: "#", title: "Trips" },
@@ -11,7 +18,20 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <span className="navbar__logo">travello</span>
-      <ul className={"navbar__list"}>
+      {menuClicked ? (
+        <FiX size={25} className={"navbar__menu"} onClick={toggleMenuClick} />
+      ) : (
+        <FiMenu
+          size={25}
+          className={"navbar__menu"}
+          onClick={toggleMenuClick}
+        />
+      )}
+      <ul
+        className={
+          menuClicked ? "navbar__list" : "navbar__list navbar__list--active"
+        }
+      >
         {navbarLinks.map((item, index) => {
           return (
             <li className="navbar__item" key={index}>
