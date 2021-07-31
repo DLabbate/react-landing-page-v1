9d8d3c6cf989f2644050397831e71860b04a5d82
Domenic Labbate <domeniclabbate@gmail.com>
2021-07-30 22:49:40 -0400

    implemented onScroll animation

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./Slider.css";
+import { useInView } from "react-intersection-observer";
 
 const Slider = ({ imageSrc, title, subtitle, flipped }) => {
+  const { ref, inView } = useInView({
+    /* Optional options */
+    threshold: 0.5,
+  });
+
   if (!flipped) {
     return (
-      <div className={"slider"}>
+      <div className={inView ? "slider slider--zoom" : "slider"} ref={ref}>
         <img src={imageSrc} alt="" className="slider__image" />
         <div className="slider__content">
           <h1 className="slider__title">{title}</h1>
@@ -14,7 +20,7 @@ const Slider = ({ imageSrc, title, subtitle, flipped }) => {
     );
   } else {
     return (
-      <div className={"slider"}>
+      <div className={inView ? "slider slider--zoom" : "slider"} ref={ref}>
         <div className="slider__content">
           <h1 className="slider__title">{title}</h1>
           <p>{subtitle}</p>
