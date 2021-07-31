f83fac343bbb06ef405ddb83e99db98cc5ea69c1
Domenic Labbate <domeniclabbate@gmail.com>
2021-07-30 21:35:38 -0400

    created Hero section

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import travel_01 from "./assets/travel-01.jpg";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hero imageSrc={travel_01} />
     </div>
   );
 }
